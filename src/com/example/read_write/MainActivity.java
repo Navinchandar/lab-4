39a5d23a714e58c6990e439ad555c60b68ed6d59
ITLAB <ITLAB@IT>
2017-02-20 15:18:03 +0530

    3rd

--- src/com/example/read_write/MainActivity.java
+++ src/com/example/read_write/MainActivity.java
@@ -4,15 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+	EditText ed_Text;
+	EditText ed_Content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String path = getPreferences(MODE_PRIVATE).getString("fpath", "/sdcard/nav_file1"); 
+        ed_Text = (EditText) findViewById(R.id.ed_Text);
+        ed_Text.setText(path);
+        ed_Content = (EditText) findViewById(R.id.ed_content);
     }
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
