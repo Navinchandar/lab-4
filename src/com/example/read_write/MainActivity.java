6d4af8e610b9fc5ecb59e6248948316b638c98bb
ITLAB <ITLAB@IT>
2017-02-20 15:37:58 +0530

    4th

--- src/com/example/read_write/MainActivity.java
+++ src/com/example/read_write/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.read_write;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -19,8 +29,78 @@ public class MainActivity extends Activity {
         ed_Text = (EditText) findViewById(R.id.ed_Text);
         ed_Text.setText(path);
         ed_Content = (EditText) findViewById(R.id.ed_content);
-    }
+      
+  
+    OnClickListener saveClickListener = new OnClickListener() {			
+		@Override
+		public void onClick(View v) {
+			File file = new File(ed_Text.getText().toString());
+			FileWriter writer=null;
+			try {
+				writer = new FileWriter(file);
+				
+				/** Saving the contents to the file*/
+				writer.write(ed_Content.getText().toString());
+				
+				/** Closing the writer object */
+				writer.close();
+				
+				
+				/** Getting sharedpreferences object to store the path of last saved file */
+				SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+				
+				/** Setting the last saved file's path in the shared preference */
+		        editor.putString("fpath", file.getPath());
+		        
+		        /** Save the path to the shared preference */
+		        editor.commit();
+				
+				Toast.makeText(getBaseContext(), "Successfully saved", Toast.LENGTH_SHORT).show();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	};
+	
+	
+	/** Defining click listener event for the button btn_save 
+     * of the layout activity_main
+     * */
+    OnClickListener readClickListener = new OnClickListener() {			
+		@Override
+		public void onClick(View v) {
+			File file = new File(ed_Text.getText().toString());
+							
+			String strLine="";
+			StringBuilder text = new StringBuilder();
+			
+			try {
+				FileReader fReader = new FileReader(file);
+				BufferedReader bReader = new BufferedReader(fReader);
+				
+				/** Reading the contents of the file , line by line */
+				while( (strLine=bReader.readLine()) != null  ){
+					text.append(strLine+"\n");						
+				}
+				
+				Toast.makeText(getBaseContext(), "Successfully loaded", Toast.LENGTH_SHORT).show();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			ed_Content.setText(text);
+		}
+	};
+	  Button b_read = (Button) findViewById(R.id.b_read);
+      b_read.setOnClickListener(saveClickListener);
+    Button btnRead = (Button) findViewById(R.id.b_write);
     
+ 
+    btnRead.setOnClickListener(readClickListener);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
